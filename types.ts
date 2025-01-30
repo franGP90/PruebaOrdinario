334c813d2dfd08390f9d9973ca193d7d6f8671f3
kikogp <fgonzalezp3@alumnos.nebrija.es>
2025-01-30 16:32:14 +0100

    3

---
+++ types.ts
@@ -0,0 +1,13 @@
+export type Comportamiento = 'bueno' | 'malo';
+
+export type Nino= {
+  nombre: string;
+  comportamiento: Comportamiento;
+  ubicacion: string; // ID de la ubicación
+}
+
+export type Ubicacion= {
+  nombre: string;
+  coordenadas: number;
+  numNinosBuenos: number;
+}
