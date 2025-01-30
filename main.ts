6107cd0f1a5230e135efec259d12511a942e2318
kikogp <fgonzalezp3@alumnos.nebrija.es>
2025-01-30 16:17:52 +0100

    2

--- main.ts
+++ main.ts
@@ -1,8 +1,39 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import { MongoClient } from 'mongodb'
+import {} from "./utils.ts"
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+const url = 'mongodb+srv://fgonzalezp3:fgonzalezp3@kikoc.v6p0n.mongodb.net/?retryWrites=true&w=majority&appName=kikoC';
+const client = new MongoClient(url);
+
+//const dbName = '';
+
+await client.connect();
+console.log('Connected successfully to server');
+/*const db = client.db(dbName);
+
+const Collection = db.collection('');*/
+
+/*const handler = async (req: Request): Promise<Response> => {
+  const url = new URL(req.url);
+  const method = req.method;
+  const path = url.pathname;
+
+  if (method === "POST" && path === "/") {
+   
+  }
+
+  if (method === "GET" && path === "/ninos/buenos") {
+    
+  }
+
+  if (method === "GET" && path === "/ninos/malos") {
+
+  }
+
+  if (method === "GET" && path === "/entregas") {
+  
+  }
+
+  return new Response("Not found", { status: 404 });
+};
+
+Deno.serve({ port: 3000 }, handler);*/
